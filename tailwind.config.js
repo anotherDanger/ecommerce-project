0378e047b17af48d7f231c5102c28602bfbd5e99
Andhika Danger <andhikadanger666@gmail.com>
2025-01-17 15:47:38 +0700

    adding components

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      colors: {
+        customNavbarColor: '#9AA6B2',
+        customBannerColor: '#BCCCDC'
+      }
+    },
+  },
+  plugins: [],
+}
+
